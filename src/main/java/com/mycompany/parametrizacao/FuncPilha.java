2874e3cc64291a97409490fbca8197d5f15e2f2e
Hugo Machado <92072405+Hugo-Machado02@users.noreply.github.com>
2023-12-11 01:00:35 -0300

    :rocket: DEPLOY: Commit Inicial

--- src/main/java/com/mycompany/parametrizacao/FuncPilha.java
+++ src/main/java/com/mycompany/parametrizacao/FuncPilha.java
@@ -10,13 +10,11 @@ package com.mycompany.parametrizacao;
  */
 public class FuncPilha {
     Celula topo;
-    int status = 1;
+    Boolean status = true;
     
     void push(String ele){
-        if(ele == "{" || ele == "[" || ele == "("){
-            Celula nova = new Celula();
-            nova.setDados(ele);
-            nova.setAnterior(this.topo);
+        if(ele.equalsIgnoreCase("{") || ele.equalsIgnoreCase("[") || ele.equalsIgnoreCase("(")){
+            Celula nova = new Celula(ele, this.topo);
             this.topo = nova;
         }else{
             pop(ele);
@@ -25,25 +23,18 @@ public class FuncPilha {
     
     void pop(String ele){
         if(this.topo != null){
-            if(ele == "}" && this.topo.getDados() == "{"){
-                System.out.println("Removendo Chaves da Pilha");
+            if(ele.equalsIgnoreCase("}") && this.topo.getDados().equalsIgnoreCase("{")){
                 this.topo = this.topo.getAnterior();
             }
-            else if(ele == "]" && this.topo.getDados() == "["){
-                System.out.println("Removendo Colchetes da Pilha");
+            else if(ele.equalsIgnoreCase("]") && this.topo.getDados().equalsIgnoreCase("[")){
                 this.topo = this.topo.getAnterior();
             }
-            else if(ele == ")" && this.topo.getDados() == "("){
-                System.out.println("Removendo Parênteses da Pilha");
+            else if(ele.equalsIgnoreCase(")") && this.topo.getDados().equalsIgnoreCase("(")){
                 this.topo = this.topo.getAnterior();
             }
             else{
-                System.out.println("Expressão Está Incorreta");
-                this.status = 0;
+                this.status = false;
             }
-        }
-        else{
-            System.out.println("Expressão Está Correta");
         }
     }
     
@@ -60,7 +51,11 @@ public class FuncPilha {
         return(tamanho);
     }
     
-    int getStatus(){
-        return(this.status);
+    String verificarExp(){
+        if(this.status == true && tamanhoPilha() == 0){
+            return("Expressão Correta");
+        }else{
+            return("Expressão Incorreta");
+        }
     }
 }
